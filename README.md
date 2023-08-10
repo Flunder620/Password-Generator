096409410de0f04cbcc3747d0af2c3c96aa0cfa5
Flunder620 <jguzman12310@yahoo.com>
2023-08-10 18:16:44 -0500

    added generatePassowrd function

---
+++ README.md
@@ -0,0 +1,14 @@
+# 02-challenge-Portfolio
+
+## Description
+
+This a password generator. When you click the button it will give you a set of options and depending on the options it will generate a password for you.
+
+## Mock-Up
+
+The following image shows the web application's appearance and functionality: 
+![This shows what home page will look like](./assets/)
+
+## Deployment Link
+
+
--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -9,6 +10,41 @@ function writePassword() {
   passwordText.value = password;
 
 }
-
+var lowercaseCharacters = 'abcdefghijklmnopqrstuvwxyz'.split('')
+var uppercaseCharacters = 'abcdefghijklmnopqrstuvwxyz'.toLocaleUpperCase().split('')
+var numberCharacters = '0123456789'.split('')
+var symbolCharacters = '!@#$%^&*()'.split('')
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+function generatePassword(){
+  var passwordOption = []
+  var finalPassword = []
+  var passLength = prompt("How many characters would you like?")
+  var hasLowercase = confirm("Would you like lowercase letters?")
+  var hasUppercase = confirm("Would you like uppercase letters?")
+  var numbers = confirm("Would you like numbers?")
+  var symbols = confirm("Would you like symbols?")
+  if (hasLowercase === true){
+    passwordOption = passwordOption.concat(lowercaseCharacters)
+  }
+  if (hasUppercase === true){
+    passwordOption = passwordOption.concat(uppercaseCharacters)
+  }
+  if (numbers === true){
+    passwordOption = passwordOption.concat(numberCharacters)
+  }
+  if (symbols === true){
+    passwordOption = passwordOption.concat(symbolCharacters)
+  }
+  if (symbols === false && hasLowercase === false && hasUppercase === false && numbers === false){
+    alert("Please choose at least one password option")
+    return
+  }
+  for(i=0; i < passLength; i++){
+    finalPassword.push(passwordOption[Math.floor(Math.random()*passwordOption.length)])
+  }
+  console.log(passwordOption, passwordOption.length)
+  console.log(finalPassword)
+  return finalPassword.join('')
+}
